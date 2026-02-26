d9ac24aea5fd0009f52e398773016e313b1fd632
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-02-26 08:07:55 +0000

    feat: generate simple divs

--- main.js
+++ main.js
@@ -1,33 +1,63 @@
 "use strict";
-//root node
 
 function generateColumn(title) {
   return {
     title: title,
-    type: "div",
+    type: "column",
+    element: "div",
     props: {},
     children: [],
   };
 }
+
 function generateTask(title = "", content = "") {
   return {
     title: title,
+    type: "task",
     content: content,
-    type: "div",
+    element: "div",
     props: {},
+    children: [],
   };
 }
-
-const columns = [
-  generateColumn("to-do"),
-  generateColumn("in-progress"),
-  generateColumn("finished"),
-];
 
 function addTask(column, title, content) {
   let task = generateTask(title, content);
   column.children.push(task);
 }
-addTask(columns[0], "Wash Dishes", "need to wash dishes ASAP");
 
-console.log(columns);
+function convert(node) {
+  const element = document.createElement(node.element);
+  const title =
+    node.type === "column"
+      ? document.createElement("h2")
+      : document.createElement("h4");
+  title.textContent = node.title;
+  element.append(title);
+  console.log(node.children);
+  if (node.children != undefined) {
+    element.append(...node.children.map(convert));
+  }
+  return element;
+}
+
+function updateDOM() {
+  if (elements == undefined) {
+    elements = vDOM.map(convert);
+    document.body.append(...elements);
+  } else {
+    prevVDOM = [...vDOM];
+    vDOM = ["to-do", "in-progress", "finished"].map(generateColumn);
+    diff(prevVDOM, vDOM);
+  }
+}
+
+function diff(previousVOM, currentVDOM) {
+  //diffing function **TODO**
+}
+let elements, vDOM, prevVDOM;
+vDOM = ["to-do", "in-progress", "finished"].map(generateColumn);
+
+addTask(vDOM[0], "Wash Dishes", "need to wash dishes ASAP");
+console.log(vDOM);
+updateDOM();
