97f86ee2ff3c5f0172eaed329b74609d19065142
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-03-01 20:59:43 +0000

    WIP: trying to organize the codebase

---
+++ generators/Element.js
@@ -0,0 +1,43 @@
+const Board = new Map();
+
+class Element {
+  constructor(name, type, parent = null) {
+    this.id = crypto.randomUUID();
+    this.name = name;
+    this.type = type;
+    this.parent = parent;
+    this.parent.children.push(this);
+    this.children = [];
+    this.registerElement(this.id, this);
+  }
+  registerElement(id, obj) {
+    Board.set(id, obj);
+  }
+  addChild(child) {
+    this.children.push(child);
+  }
+  removeChild(childID) {
+    this.children = this.children.filter((child) => child.id !== childID);
+  }
+  moveTo(newParentID) {
+    const newParent = Board.get(newParentID);
+    this.parent.removeChild(this.id);
+    this.parent = newParent;
+    newParent.addChild(this);
+  }
+  rename(newName) {
+    this.name = newName;
+  }
+}
+
+class Column extends Element {
+  constructor(name) {
+    super(name, "column");
+  }
+}
+class Task extends Element {
+  constructor(name, content, parentColumn) {
+    super(name, "task", parentColumn);
+    this.content = content;
+  }
+}
