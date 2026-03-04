5929ff311f413be26bf46db79b7413b119f192af
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-03-04 10:59:22 +0000

    simple node server that serves the project files

---
+++ server.js
@@ -0,0 +1,28 @@
+import http from "http";
+import fs from "fs";
+import path from "path";
+
+const server = http.createServer((req, res) => {
+  const filePath = req.url === "/" ? "./index.html" : "." + req.url;
+  const fileExtension = path.extname(filePath);
+
+  let contentType = "text/plain";
+  if (fileExtension === ".html") {
+    contentType = "text/html";
+  } else if (fileExtension === ".css") {
+    contentType = "text/css";
+  } else if (fileExtension === ".js") {
+    contentType = "text/javascript";
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.statusCode = 404;
+      res.end("Not Found");
+    } else {
+      res.setHeader("content-type", contentType);
+      res.end(content);
+    }
+  });
+});
+server.listen(3000);
