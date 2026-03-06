95563d05add8a1590e9fba718cdf1cbe6ed192eb
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-03-06 23:15:02 +0000

    feat: load and save function

--- src/Board.js
+++ src/Board.js
@@ -8,6 +8,35 @@ export default function Board() {
     register(id, element) {
       this.elements.set(id, element);
     },
+    load: async function () {
+      const response = await fetch("/root");
+      if (!response.ok) {
+        alert("Data load failed");
+        return;
+      }
+      let { root, elements } = await response.json();
+      if (this.root.length <= 0) {
+        this.root = root;
+        this.elements = new Map(elements);
+      } else {
+        //ask User to confirm or save changes ****
+      }
+    },
+    save: async function () {
+      const response = await fetch("/save", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          root: this.root,
+          elements: Array.from(this.elements.entries()),
+        }),
+      });
+      if (!response.ok) {
+        alert("Data save failed");
+        return;
+      }
+      console.log("Data saved in database");
+    },
     createTask(parentColumnID, name, content) {
       const task = Task(parentColumnID, name, content);
       this.register(task.id, task);
@@ -37,36 +66,38 @@ export default function Board() {
       }
       this.elements.delete(id);
     },
-    shift(to, id) {
+    shiftTask(to, id) {
       const element = this.elements.get(id);
       const target = this.elements.get(to);
 
-      if (element.type == "task") {
-        const originalColumn = this.elements.get(element.parentColumnID);
-        originalColumn.children = originalColumn.children.filter(
-          (a) => a != element.id,
-        );
+      const originalColumn = this.elements.get(element.parentColumnID);
+      originalColumn.children = originalColumn.children.filter(
+        (a) => a != element.id,
+      );
 
-        if (target.type == "column") {
-          element.parentColumnID = to;
-          target.children.push(element.id);
-          //
-        } else if (target.type == "task") {
-          const targetParent = this.elements.get(target.parentColumnID);
-          element.parentColumnID = targetParent.id;
-          const targetIndex = targetParent.children.findIndex(
-            (a) => a == target.id,
-          );
-          targetParent.children.splice(targetIndex, 0, element.id);
-        }
-      } else if (element.type == "column") {
-        this.root = this.root.filter((a) => a != element.id);
-        const targetIndex =
-          target.type == "column"
-            ? this.root.findIndex((a) => a == target.id)
-            : this.root.findIndex((a) => a == target.parentColumnID);
-        this.root.splice(targetIndex, 0, id);
+      if (target.type == "column") {
+        element.parentColumnID = to;
+        target.children.push(element.id);
+        //
+      } else if (target.type == "task") {
+        const targetParent = this.elements.get(target.parentColumnID);
+        element.parentColumnID = targetParent.id;
+        const targetIndex = targetParent.children.findIndex(
+          (a) => a == target.id,
+        );
+        targetParent.children.splice(targetIndex, 0, element.id);
       }
+    },
+    shiftColumn(to, id) {
+      const element = this.elements.get(id);
+      const target = this.elements.get(to);
+
+      this.root = this.root.filter((a) => a != element.id);
+      const targetIndex =
+        target.type == "column"
+          ? this.root.findIndex((a) => a == target.id)
+          : this.root.findIndex((a) => a == target.parentColumnID);
+      this.root.splice(targetIndex, 0, id);
     },
   };
   return board;
