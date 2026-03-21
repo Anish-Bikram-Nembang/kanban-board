4f950053491e7ae80eaf168aaac82b5064e0bce8
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-03-21 15:56:25 +0000

    added a pool~

--- src/db.js
+++ src/db.js
@@ -1,4 +1,17 @@
 import fs from "node:fs/promises";
+import pg from "pg";
+import process from "process";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new pg.Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 const DB_PATH = new URL("../data.json", import.meta.url).pathname;
 
