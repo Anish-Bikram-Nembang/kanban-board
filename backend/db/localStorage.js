35b1f3ed0f78e793a09d77608e5123113cb2cce1
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-03-24 15:41:37 +0000

    feat: renaming backend to server

--- backend/db/localStorage.js
+++
@@ -1,8 +0,0 @@
-export default lStorage = {
-  save: async function (board) {
-    localStorage.setItem(board.name, JSON.stringify(board.db));
-  },
-  load: async function (board) {
-    return localStorage.getItem(board.name);
-  },
-};
