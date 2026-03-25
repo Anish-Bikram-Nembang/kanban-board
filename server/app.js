8297152f8b642742782953dd60fa0319ee765516
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-03-25 06:43:25 +0000

    feat: using express to simplify backend, add helmet, cors

--- server/app.js
+++ server/app.js
@@ -1,48 +1,11 @@
-import http from "http";
-import fs from "fs/promises";
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
 
-import { getDB, saveDB } from "./db.js";
+const app = express();
 
-export default app = http.createServer(async (req, res) => {
-  if (req.url == "/getdata") {
-    res.statusCode = 200;
-    res.setHeader("content-type", "application/json");
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
-    const response = await getDB();
-    res.end(response);
-  } else if (req.url == "/savedata") {
-    let data = "";
-    req.on("data", (chunk) => {
-      data += chunk.toString();
-    });
-    req.on("end", async () => {
-      await saveDB(data);
-      res.end();
-    });
-  } else if (req.url == "/") {
-    const file = await fs.readFile("./index.html");
-    res.setHeader("content-type", "text/html");
-    res.statusCode = 200;
-    res.end(file);
-  } else if (req.url == "/main.js") {
-    const file = await fs.readFile("./main.js");
-    res.setHeader("content-type", "text/javascript");
-    res.statusCode = 200;
-    res.end(file);
-  } else if (req.url == "/style.css") {
-    const file = await fs.readFile("./style.css");
-    res.setHeader("content-type", "text/css");
-    res.statusCode = 200;
-    res.end(file);
-  } else {
-    try {
-      const file = await fs.readFile(`.${req.url}`);
-      res.setHeader("content-type", "text/javascript");
-      res.statusCode = 200;
-      res.end(file);
-    } catch (err) {
-      res.writeHead(404);
-      res.end("Not found!");
-    }
-  }
-});
+export default app;
